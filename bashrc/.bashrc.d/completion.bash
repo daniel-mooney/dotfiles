2a8d3a751fe24454e400ff8a4e15950adb22fab8
Daniel Mooney <daniel.mooney180@gmail.com>
2026-04-16 17:23:51 +1000

    Addition of bashrc
    TODO: additional autocompletion features

---
+++ bashrc/.bashrc.d/completion.bash
@@ -0,0 +1,7 @@
+# fzf 0.44.x bash integration
+if [ -f /usr/share/doc/fzf/examples/key-bindings.bash ]; then
+  . /usr/share/doc/fzf/examples/key-bindings.bash
+fi
+if [ -f /usr/share/doc/fzf/examples/completion.bash ]; then
+  . /usr/share/doc/fzf/examples/completion.bash
+fi
