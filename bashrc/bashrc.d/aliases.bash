ff0140c387cc1ed6489eec8eae15c0e333dcf520
Daniel Mooney <daniel.mooney180@gmail.com>
2026-04-19 17:15:23 +1000

    Updated ls alias

--- bashrc/bashrc.d/aliases.bash
+++ bashrc/bashrc.d/aliases.bash
@@ -3,3 +3,4 @@ alias bat="batcat"
 alias y="yazi"
 
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+alias ls="ls --group-directories-first --color=auto"
--- bashrc/bashrc.d/appearance.bash
+++ bashrc/bashrc.d/appearance.bash
@@ -5,7 +5,7 @@ eval "$(starship init bash)"
 # enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-    alias ls='ls --color=auto'
+    # alias ls='ls --color=auto'
     #alias dir='dir --color=auto'
     #alias vdir='vdir --color=auto'
 
